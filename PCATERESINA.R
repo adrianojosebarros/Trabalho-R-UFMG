15b1baabd3427e9f775ca5b0b2bd8dc3caeaa16d
adrianojosebarros <62964789+adrianojosebarros@users.noreply.github.com>
2023-11-30 20:10:41 -0300

    Update PCATERESINA.R

--- PCATERESINA.R
+++ PCATERESINA.R
@@ -1,84 +1,93 @@
-library(readr)
-library(dplyr)
-library(FactoMineR)
-library(factoextra)
 library(tidyverse) # Para manipular os dados
 library(stats) # Para PCA
+library(factoextra) # Para criar alguns gráficos
 library(vegan)
-library(ggcorrplot)
+library(ggplot2)
+library(dplyr)
+install.packages("psych")
+library(psych)
 
 
 
+Tn2 <- SES_Teresina  %>% select (- c (1,2,3,4,5))
+cor(Tn2)
+ggcorrplot(Tn2, lab = T)
+ggpairs(Tn2, lower = list(continuous = "smooth"))
+ggpairs(Tn2, columns = 1:13, ggplot2::aes(colour= "P_DESEMP"))
+summary(Tn2)
 
-Tn <- Teresina  %>% select (- c (1,2,3))
-#Inferter linhas por colunas
-TTN<- t(Tn)
 
-#Calcular PCA
-PCA(X=Tn , graph = F , scale.unit = T , ncp = ncol(Tn))
-#respca 
-resPCA <- PCA(X=Tn , graph = F , scale.unit = T , ncp = ncol(Tn))
+# Padronizar
+PCA(X = Tn2 , graph = F , scale.unit = T , ncp = ncol(Tn2))
+RESPCA <- PCA(X=Tn , graph = F , scale.unit = T , ncp = ncol(Tn)) # Respostas da PCA
 
-#SUBconjunto de dados 
-#PCA INDIVIDUAL
-
-PCAIND <- get_pca_ind(resPCA)
-
+# Calcular para variaveis Matriz transposta 
+MATRAS<- t(Tn2)
+# dados individuos
+PCAIND <- get_pca_ind(RESPCA)
 #SUBconjunto de dados 
 #PCA Cordenadas
-resPCA$ind$coord
-
-
+RESPCA$ind$coord
 # convert dada frem
-as.data.frame(resPCA$ind$coord) %>% select(1:15)
-PCS <- as.data.frame(resPCA$ind$coord) %>% select(1:15) %>% setNames(paste0('pc.', 1:15))
+as.data.frame(RESPCA$ind$coord) %>% select(1:13)
+
+PCSN <- as.data.frame(resPCA$ind$coord) %>% select(1:13) %>% setNames(paste0('pc.', 1:13))
+
 
 # juntar no conjunto de dados Principal e PCA
 
-DADOSN<- cbind(Tn,PCS)
+DADOSN2<- cbind(Tn2,PCSN)
+
+AutoValores<-RESPCA$eig
+
+#Grafico
+fviz_eig(RESPCA , addlabels = T, xlab = "Dimensoes", ylab = "Variancia Explicada
+         [%]",main = "Grafico de variancias")
+
+fviz_pca_biplot(RESPCA)
+##########################################################################################################
 
 
-# Auto valores 
-resPCA$eig
-autoValores<-resPCA$eig
+mvn(Tn2, mvnTest = "hz", desc = FALSE, multivariatePlot = "qq")
 
-# Crir grafico auto valores
+cortest.bartlett(Tn2)
+KMO(Tn2)   #KMO Test Kaiser provided the following values for interpreting the results:
 
-fviz_eig(resPCA, addlabels = T,
-         xlab = 'Dimensoes', ylab = 'Variancia' , main = 'Grafico Variancia') 
-
-#Grafico variancia acumulada 
-as.data.frame(autoValores)
-
-autovaloresDF<-as.data.frame(autoValores)%>% mutate(dims = as.factor(1:nrow(.)),
-                     v = round(' cumulative percentage of variance ',1 ))
-
-
- # matri de correlação 
-mcor <- resPCA$var$coord
-mcor<- as.data.frame(resPCA$var$coord) 
-ggcorrplot(mcor, lab = T)
-ggpairs(mcor, columns = 1:15, ggplot2::aes(colour= "Din.1"))
-
-ggpairs(mcor, lower = list(continuous = "smooth"))
-
-  
-fviz_pca_var(resPCA)   
-
-fviz_pca_ind(resPCA,
-             col.ind = "cos2", #Cor pela qualidade de representação
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE, # Texto não sobreposto
-             legend.title = "Representação"
-             
-             
-             fviz_pca_biplot(resPCA, repel = TRUE,
-                             col.var = "#2E9FDF", # cor das variáveis
-                             col.ind = "#696969"  # cor dos automoveis
+princomp(Tn2,cor=TRUE)
+MAF<-princomp(Tn2,cor=TRUE)
+summary(MAF)
+screeplot(MAF)
+plot(MAF,type="lines")
 
 
 
+loads <- MAF$scores
+loads <- MAF$loadings
+
+fa.diagram(loads)
+loads2 <- fit$loadings[,1:2]
+fa.diagram(loads2)
+
+loads3 <- fit$loadings[,3:4]
+fa.diagram(loads3)
+
+loads4 <- fit$loadings[,5:6]
+fa.diagram(loads4)
+
+loads5 <- fit$loadings[,7:8]
+fa.diagram(loads5)
+
+loads6 <- fit$loadings[,9:10]
+fa.diagram(loads6)
+
+loads7 <- fit$loadings[,11:12]
+fa.diagram(loads7)
+
+loads8 <- fit$loadings[,13:14]
+fa.diagram(loads8)
+
+loads9 <- fit$loadings[6-15]
+fa.diagram(loads9)
 
 
 
-            
